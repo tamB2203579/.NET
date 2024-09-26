9e6337ee3974bef61c82cac5631bbeccbb606f22
tamB2203579 <145105134+tamB2203579@users.noreply.github.com>
2024-09-26 08:22:20 +0700

    Add files via upload

---
+++ Blazor/BlazorApp/Components/Pages/CounterBase.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp.Components.Pages;
+
+public class CounterBase:ComponentBase{
+    public int currentCount = 0;
+    [Parameter]
+    public int IncrementAmount {get ; set; } = 1;
+    public void IncrementCount(){
+        currentCount++;
+    }
+}
